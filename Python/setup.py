0f30d1615dcc0641991ca2f47e7fb60d583a23eb
Olle Norelius <olle.norelius@infotiv.se>
2018-03-13 11:20:52 +0100

    Added setup.py

---
+++ Python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='autonomous',
+    version='1.0dev',
+    packages=['autonomous', 'common'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
